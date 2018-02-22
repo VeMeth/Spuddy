ff1640dcf8981d9ab68600e475348b5c64c54d68
VeMeth <noerd666@gmail.com>
2018-02-22 01:38:49 +0100

    Progress

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var dotenv = require("dotenv");
 dotenv.load();
 const token = process.env.token;
-const spudchan = "spuds";
+const spudchan = process.env.spudchan;
 
 var rqplayers = "2";
 
@@ -21,7 +21,10 @@ client.on("ready", () => {
 
 
 // Commands
-var spudders = [];
+var spudders = [{
+	user:"",
+	timestamp: ""
+}];
 client.on("message" , (message) => {
 	if(message.author.bot) return;
 	if(message.content.indexOf(prefix) !== 0) return;
@@ -31,20 +34,31 @@ client.on("message" , (message) => {
 
 
 
-	// SIGNUP 
-	if (command  === "spud" && message.channel.name == spudchan) {
+	// SIGNUP
 	
-		if (!spudders.includes(message.author)) {
-			spudders.push(message.author);
-			message.reply("You were signed up to a spud");
+	if (command  === "spud" && message.channel.name == spudchan) {
+		
+		if (!spudders.includes(message.author.nickname)) {					
+			spudders.push(
+				{
+					user : 			message.author,
+					timestamp : 	Date.now()
+				}
+			);
+			message.channel.send("You were signed up to a spud");
 		}
+		
+		
+		
 		else {
 			message.channel.send("You already signed up for this round");
 		}
-	
+		
 		console.log(spudders.length);
 		if (spudders.length >= rqplayers ) {
-			message.channel.send("SPUD IS RDY");
+			var hostRole = message.channel.guild.roles.find("name", "hosts");
+			
+			message.channel.send("SPUD IS RDY " + hostRole);
 		}
 	}
   
@@ -52,12 +66,14 @@ client.on("message" , (message) => {
 	if (command  === "sup" && message.channel.name == spudchan) {
 		var list = "";
 		if (spudders.length == 0)
-		{message.reply("Nobody is siged up yet");}
+		{
+			message.reply("Nobody is siged up yet");
+		}
 		else {
 			for (var spud of spudders)  {
-				list =  spud.username;
+				list =  spud.user.username;
 				console.log(spud);
-				message.channel.send("\n" +spudders.length + "/" + rqplayers + " | " + list);
+				message.channel.send("\n" +spudders.length + "/" + rqplayers + " | " + list  + " " + spud.timestamp);
 			}	  
 		}
 
@@ -67,7 +83,21 @@ client.on("message" , (message) => {
 		rqplayers = args[0];
 		message.channel.send("Spud size set to: " + rqplayers);
 	}
-  
+ 
+	if (command  === "exp") {
+		for (var entries of spudders)  {
+			var name =  entries.user.username;
+			var ts = entries.timestamp;
+			if ((Date.now() - ts) >= 60000 ) {
+				message.channel.send("Sign up of: " + name + " expired");
+			}
+			else {
+				message.channel.send(name + " is still signed up");
+			}
+
+		}
+
+	}
 
 
 	if (command  === "test") {
